bb7a9e15c5e453ca5131c1b0d70d90b8933b12e7
clinyong <clinyong@gmail.com>
2018-05-01 11:35:15 +0800

    Do some check

--- interview-offer/chp2/06-rebuild-tree.js
+++ interview-offer/chp2/06-rebuild-tree.js
@@ -7,24 +7,35 @@ const { Node } = require("../../data-structures/05-binary-tree");
  * @param {number[]} middleOrderList
  */
 module.exports = function rebuildTree(preOrderList, middleOrderList) {
+    if (
+        !Array.isArray(preOrderList) ||
+        !Array.isArray(middleOrderList) ||
+        preOrderList.length !== middleOrderList.length
+    ) {
+        return null;
+    }
+
     const root = preOrderList[0];
-    const Node = new Node(root);
+    const node = new Node(root);
 
     const rootInexInMiddle = middleOrderList.findIndex(item => item === root);
 
     const leftTreeLen = rootInexInMiddle - 0;
+    const rightTreeLen = preOrderList.length - leftTreeLen - 1;
 
-    // 找出所有左子树
-    Node.left = rebuildTree(
-        preOrderList.slice(1, leftTreeLen + 1),
-        middleOrderList.slice(0, rootInexInMiddle)
-    );
+    if (leftTreeLen > 0) {
+        node.left = rebuildTree(
+            preOrderList.slice(1, leftTreeLen + 1),
+            middleOrderList.slice(0, rootInexInMiddle)
+        );
+    }
 
-    // 找出所有右子树
-    Node.right = rebuildTree(
-        preOrderList.slice(leftTreeLen + 1),
-        middleOrderList.slice(rootInexInMiddle + 1)
-    );
+    if (rightTreeLen > 0) {
+        node.right = rebuildTree(
+            preOrderList.slice(leftTreeLen + 1),
+            middleOrderList.slice(rootInexInMiddle + 1)
+        );
+    }
 
-    return Node;
+    return node;
 };
--- interview-offer/chp2/__test__/06-rebuild-tree.test.js
+++ interview-offer/chp2/__test__/06-rebuild-tree.test.js
@@ -6,6 +6,16 @@ import {
 } from "../../../data-structures/05-binary-tree";
 import rebuildTree from "../06-rebuild-tree";
 
+test("invalid input", t => {
+    const head1 = rebuildTree(null, []);
+    const head2 = rebuildTree([], null);
+    const head3 = rebuildTree([1], []);
+
+    t.is(head1, null);
+    t.is(head2, null);
+    t.is(head3, null);
+});
+
 test("rebuild binary tree from pre and middle traverse", t => {
     const preOrderList = [1, 2, 4, 7, 3, 5, 6, 8];
     const middleOrderList = [4, 7, 2, 1, 5, 3, 8, 6];
